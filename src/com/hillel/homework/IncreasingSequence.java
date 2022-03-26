368fc8c339500be3dd1acfac3ba2377c21a8f1e4
demesup <guzhansofia@gmail.com>
2022-03-26 09:57:39 +0200

    Add to homework

---
+++ src/com/hillel/homework/IncreasingSequence.java
@@ -0,0 +1,49 @@
+package com.hillel.homework;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class IncreasingSequence {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Введите количество чисел ");
+
+
+        int length = scanner.nextInt();
+
+        if (length > 1) {
+            int[] array = new int[length];
+            for (int i = 0; i < array.length; i++) {
+                System.out.printf("Введите %d число \n ", i);
+                array[i] = scanner.nextInt();
+            }
+
+            System.out.println(Arrays.toString(array));
+
+            increasingSequence(array);
+        } else {
+            System.out.println("Для последовательности нужно больше 2 чисел");
+        }
+
+    }
+
+    public static void increasingSequence(int[] array) {
+        boolean check = true;
+
+        for (int i = 1; i < array.length; i++) {
+            if (array[i] <= array[i - 1]) {
+                check = false;
+                break;
+            }
+        }
+
+        if (check) {
+            System.out.println("Массив является возрастающей последовательностью");
+        } else {
+            System.out.println("Массив не является возрастающей последовательностью");
+        }
+    }
+
+}
