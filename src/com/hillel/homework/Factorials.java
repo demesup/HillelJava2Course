e6b99b33429cf4a44f750ca47956ce1cb8c6c3c8
demesup <guzhansofia@gmail.com>
2022-03-26 09:24:34 +0200

    Add to homework

--- src/com/hillel/homework/Factorials.java
+++ src/com/hillel/homework/Factorials.java
@@ -3,15 +3,28 @@ package com.hillel.homework;
 import java.util.Scanner;
 
 public class Factorials {
+    public static int COUNT = 1;
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        System.out.println("Введите число больше нуля");
         long number = scanner.nextLong();
 
-        System.out.println(factorial(number));
+        if (number > 0 & number <= 20) {
+            System.out.println("Факторил с помощью цикла " + factorial(number));
+            System.out.println("Факторил с помощью рекурсии " + recursionFactorial(number));
+        } else if (number > 20) {
+            System.out.println("Факториал слишком большой для типа long");
+        } else {
+            System.out.println("Число меньше или равно нулю");
+        }
     }
 
+
     public static long factorial(long x) {
+        if (x == 1) {
+            return 1;
+        }
         long y = 1;
         for (int i = 1; i <= x; i++) {
             y = y * i;
@@ -20,11 +33,10 @@ public class Factorials {
     }
 
     public static long recursionFactorial(long x) {
-        long y = 1;
-        for (int i = 1; i <= x; i++) {
-            y = y * i;
+        if (x == 1) {
+            return 1;
         }
-        return y;
+        return recursionFactorial(x - 1) * x;
     }
 
 }
