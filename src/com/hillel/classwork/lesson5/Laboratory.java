83e7e177567b929a143a4c587f3a8c3e20c34e90
demesup <guzhansofia@gmail.com>
2022-04-09 14:06:44 +0300

    Add to lesson5

--- src/com/hillel/classwork/lesson5/Laboratory.java
+++ src/com/hillel/classwork/lesson5/Laboratory.java
@@ -4,32 +4,52 @@ import com.hillel.classwork.lesson5.Plant;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Locale;
 import java.util.Scanner;
 
 public class Laboratory {
+
+    public static final Scanner SCANNER = new Scanner(System.in);
+
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
+
         List<Plant> plants = new ArrayList<>();
 
-        System.out.println("Dandelion height: ");
-        int height1 = scanner.nextInt();
-        Plant dandelion = new Dandelion(height1);
-        plants.add(dandelion);
-
-        System.out.println("Cactus height: ");
-        int height2 = scanner.nextInt();
-        Plant cactus = new Cactus(height2);
-        plants.add(cactus);
-
-        System.out.println("Orange age: ");
-        int age = scanner.nextInt();
-        Plant orange = new Orange(age);
-        plants.add(orange);
-
-        for (Plant plant: plants) {
-            plant.photosynthesis();
+        String actionMessage = "Choose action: 1.create plant. 2. press any key to exit";
+        System.out.println(actionMessage);
+        String input = SCANNER.nextLine();
+        while ("create".equals(input.toLowerCase(Locale.ROOT).replaceAll(" ",""))) {
+            Plant plant = createPlant();
+            if (plant != null) plants.add(plant);
+            System.out.println(actionMessage);
+            input = SCANNER.nextLine();
         }
 
-
+        for (Plant plant : plants) {
+            plant.photosynthesis();
+        }
     }
+
+    public static Plant createPlant() {
+        System.out.println("Input plant type: orange, cactus, dandelion");
+        String plantType = SCANNER.nextLine();
+        switch (plantType.toLowerCase(Locale.ROOT).replaceAll(" ","")) {
+            case "orange":
+                System.out.println("Input orange age: ");
+                int age = Integer.parseInt(SCANNER.nextLine());
+                return new Orange(age);
+            case "cactus":
+                System.out.println("Input cactus height: ");
+                int height = Integer.parseInt(SCANNER.nextLine());
+                return new Cactus(height);
+            case "dandelion":
+                System.out.println("Input dandelion height: ");
+                int height1 = Integer.parseInt(SCANNER.nextLine());
+                return new Dandelion(height1);
+            default:
+                System.out.println("Incorrect plant type " + plantType + ". Try again");
+                return null;
+        }
+    }
+
 }
