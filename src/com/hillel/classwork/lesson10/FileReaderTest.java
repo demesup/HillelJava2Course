34e5917b60de64e334b695bd697a1584c8fe35b9
demesup <guzhansofia@gmail.com>
2022-05-28 13:50:05 +0300

    Add to file writer

--- src/com/hillel/classwork/lesson10/FileReaderTest.java
+++ src/com/hillel/classwork/lesson10/FileReaderTest.java
@@ -1,13 +1,27 @@
 package com.hillel.classwork.lesson10;
 
-import java.io.BufferedReader;
-import java.io.FileReader;
-import java.io.IOException;
+import java.io.*;
 
 public class FileReaderTest {
+
+    public static String FILE_NAME = "C:\\Users\\User\\Downloads\\readMe.txt";
+
     public static void main(String[] args) throws IOException {
-        try (BufferedReader reader = new BufferedReader(new FileReader("C:\\Users\\User\\Downloads\\readMe.txt"))) {
-            System.out.println(reader.readLine());
+        readFile();
+        writeFile("\ntext new hi");
+    }
+
+    private static void readFile() {
+        try (BufferedReader reader = new BufferedReader(new FileReader(FILE_NAME))) {
+            while (reader.ready())System.out.println(reader.readLine());
+        } catch (IOException e) {
+            System.out.println("ERROR: " + e.getMessage());
+        }
+    }
+
+    public static void writeFile(String text){
+        try (BufferedWriter writer = new BufferedWriter(new FileWriter(FILE_NAME, true))){
+            writer.write(text);
         } catch (IOException e) {
             System.out.println("ERROR: " + e.getMessage());
         }
